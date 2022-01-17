f6fa26371f73bface0368ce7bed1caeaf57f0957
glesenok <glesenok@yandex.ru>
2022-01-17 23:17:18 +0500

    Домашнее задание

---
+++ HW6-3.py
@@ -0,0 +1,19 @@
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'profit': wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_full_profit(self):
+        return f"{sum(self._income.values())}"
+
+manager = Position("Mikle", 'Tom', "WB", 50000, 1657922)
+print(manager.get_full_name())
+print(manager.position)
+print(manager.get_full_profit())
